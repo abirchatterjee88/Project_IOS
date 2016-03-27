782ea120b5a60a16cd5a48b5a3edc417cb190ddb
Abir Chatterjee <abir_chatterjee@hotmail.com>
2016-03-27 16:08:11 +0530

    Commit

---
+++ PHAVR/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PHAVR
+//
+//  Created by Abir Chatterjee on 27/03/16.
+//  Copyright © 2016 Abir Chatterjee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PHAVR/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PHAVR
+//
+//  Created by Abir Chatterjee on 27/03/16.
+//  Copyright © 2016 Abir Chatterjee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
